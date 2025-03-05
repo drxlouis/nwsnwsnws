36c5237d8ea66f2efe985fa9b4f09937c8d005ce
Louis Dierickx <dierickx.louis01@gmail.com>
2025-03-05 20:57:47 +0100

    format

--- server/app.ts
+++ server/app.ts
@@ -6,21 +6,16 @@ import routes from "./routes";
 const app: Application = express();
 const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
 
-// EJS als template-engine instellen
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Middleware voor layouts
 app.use(expressLayouts);
 app.set("layout", "layouts/main");
 
-// Middleware om statische bestanden te serveren
 app.use(express.static(path.join(__dirname, "/public")));
 
-// Middleware om formulierdata te verwerken
 app.use(express.urlencoded({ extended: true }));
 
-// Routes gebruiken
 app.use("/", routes);
 
 // Server starten
--- server/routes.ts
+++ server/routes.ts
@@ -1,23 +1,15 @@
 import express, { Request, Response } from "express";
 import { getNews, getNewsBySlug, addNews } from "./data/newsService";
 
-
 const router = express.Router();
 
-// Middleware to parse form data
 router.use(express.urlencoded({ extended: true }));
 
-/**
- * GET / - Laadt de homepagina
- */
 router.get("/", (req: Request, res: Response): void => {
   const news = getNews();
   res.render("index", { title: "Home Page", news });
 });
 
-/**
- * GET /nieuws/:slug - Laadt de detailpagina van een nieuwsartikel
- */
 router.get("/nieuws/:slug", (req: Request, res: Response): void => {
   const article = getNewsBySlug(req.params.slug);
   if (article) {
@@ -27,16 +19,10 @@ router.get("/nieuws/:slug", (req: Request, res: Response): void => {
   }
 });
 
-/**
- * GET /add - Laadt de pagina om een nieuwsartikel toe te voegen
- */
 router.get("/add", (req: Request, res: Response): void => {
   res.render("add", { title: "Voeg Nieuws Toe" });
 });
 
-/**
- * POST /add - Verwerkt het formulier om een nieuwsartikel toe te voegen
- */
 router.post("/add", (req: Request, res: Response): void => {
   const { title, content, date } = req.body;
   const newArticle = { title, content, date };
@@ -44,7 +30,6 @@ router.post("/add", (req: Request, res: Response): void => {
   res.redirect("/");
 });
 
-// Catch-all route for handling 404 errors
 router.use((req: Request, res: Response): void => {
   res.status(404).render("404", { title: "404" });
 });
